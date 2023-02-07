51a03ccb57b63686df1409ffd90d732bdf746630
Kathrin Jutzeler <113783939+kathrinsjutzeler@users.noreply.github.com>
2023-02-07 10:48:08 -0600

    Add files via upload

--- scripts/3_align_bwa.sh
+++ scripts/3_align_bwa.sh
@@ -17,14 +17,11 @@ mkdir /master/kbailey/sm_single_gt/results/nodups
 REF_DIR="/master/kbailey/sm_single_gt/data/reference"
 RESULTS_DIR="/master/kbailey/sm_single_gt/results"
 
-for sample in $(cat 
-/master/kbailey/sm_single_gt/data/seq_data/test_list.txt);
+for sample in $(cat /master/kbailey/sm_single_gt/data/seq_data/test_list.txt);
   do
   SAM=$(echo ${sample} | cut -f1 -d"_")
 
-  CMD="bwa mem -M $REF_DIR/GCA_000237925.5_SM_V9_genomic.fna.gz 
-$RESULTS_DIR/trimmed_fastq/${sample}_R1_001_val_1.fq.gz $RESULTS_DIR/trimmed_fastq/${sample}_R2_001_val_1.fq.gz 
-| samtools sort -o \"$RESULTS_DIR/aligned/${sample}.bam\"
+  CMD="bwa mem -M $REF_DIR/GCA_000237925.5_SM_V9_genomic.fna.gz $RESULTS_DIR/trimmed_fastq/${sample}_R1_001_val_1.fq.gz $RESULTS_DIR/trimmed_fastq/${sample}_R2_001_val_2.fq.gz | samtools sort -o \"$RESULTS_DIR/aligned/${sample}.bam\"
   picard MarkDuplicates I=\"$RESULTS_DIR/aligned/${sample}.bam\" O=\"$RESULTS_DIR/nodups/${sample}.bam\" M=\"${sample}.txt\" REMOVE_DUPLICATES=true ASSUME_SORTED=true
   samtools flagstat $RESULTS_DIR/nodups/${sample}.bam > ${sample}.flagstat;
   samtools index $RESULTS_DIR/nodups/${sample}.bam"
