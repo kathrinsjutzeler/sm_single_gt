e1d884183d5a789572fb9c48bd3e0f1326964fc4
Kathrin Jutzeler <113783939+kathrinsjutzeler@users.noreply.github.com>
2023-02-09 09:44:24 -0600

    Add files via upload

---
+++ scripts/5_HaplotypeCaller.sh
@@ -0,0 +1,43 @@
+echo """
+#!/bin/bash
+#$ -V
+#$ -cwd
+#$ -S /bin/bash
+#$ -N As_SAMPLE
+#$ -o /master/kbailey/ascaris_test/results/logs/haplo.SAMPLE_INTERVAL.log
+#$ -j y
+#$ -q all.q
+#$ -pe smp 1
+
+""" >header.qsub.sh
+
+REF_DIR="/master/kbailey/ascaris_test/data/reference"
+RESULTS_DIR="/master/kbailey/ascaris_test/results"
+
+mkdir /master/kbailey/ascaris_test/results/haplo
+mkdir /master/kbailey/temp/haplo
+
+for interval in $(cat $REF_DIR/intervals/intervals.list); do
+  for sample in $(cat /master/kbailey/ascaris_test/data/part_samples.txt); do
+    NUM=$(echo ${interval} | cut -f1 -d"-")
+    SAM=$(echo ${sample} | cut -f1 -d"-")
+	
+    CMD="gatk --java-options \"-Xmx12g\" HaplotypeCaller \
+    -R $REF_DIR/GCA_013433145.1_ASM1343314v1_genomic.fna \
+    -I \"$RESULTS_DIR/finalbam/${sample}.bam\" \
+    -O \"$RESULTS_DIR/haplo/${sample}.${NUM}.g.vcf.gz\" \
+    -ERC GVCF \
+    -L $REF_DIR/intervals/${interval} \
+    --tmp-dir /master/kbailey/temp/haplo"
+
+    cat header.qsub.sh >${SAM}_$NUM.sh
+    sed -i "s/As_SAMPLE/As_${SAM}/g" ${SAM}_${NUM}.sh
+    sed -i "s/SAMPLE/${SAM}/g" ${SAM}_${NUM}.sh
+    sed -i "s/INTERVAL/$NUM/g" ${SAM}_${NUM}.sh
+
+    echo $CMD >>${SAM}_${NUM}.sh
+
+    #qsub -V -cwd -S /bin/bash -N haplo_${sample}_${interval} -o /master/kbailey/ascaris_test/results/logs/haplo.o_${sample}_${interval}.log  ${sample}_${interval}.sh
+    qsub ${SAM}_${NUM}.sh
+  done
+done
