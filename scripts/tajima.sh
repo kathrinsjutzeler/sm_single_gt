579d0fccf112dd1ca1c8a74f526b6b6ab48b8f2c
Kathrin Jutzeler <113783939+kathrinsjutzeler@users.noreply.github.com>
2024-02-09 09:35:50 -0600

    Create tajima.sh

---
+++ scripts/tajima.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+pop=("EG" "BRE" "LE" "OR" "NMRI" "br" "tz" "ne" "sn")
+
+mkdir ~/sm_single_gt/results/tajima_v10
+
+for pop in ${pop[@]}; do
+
+vcftools --vcf annotated_snps_nuclear_CDS_v10.vcf --TajimaD 25000 --keep /master/kbailey/sm_single_gt/results/lists/${pop}.args --out /master/kbailey/sm_single_gt/results/tajima_v10/annotated_snps_nuclear_CDS_tajima_${pop};
+
+done 
